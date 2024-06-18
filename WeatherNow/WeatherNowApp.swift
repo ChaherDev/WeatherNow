e65a163794f38ea831d24f0f8111a07675948f9b
Chaher <chaher.croissance.public@gmail.com>
2024-06-18 20:43:09 +0200

    Initial Commit

---
+++ WeatherNow/WeatherNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherNowApp.swift
+//  WeatherNow
+//
+//  Created by Chaher Machhour on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
